a391d7c1216ed969d294d40b376a43c9c369965f
tubakadriye <tubakadriye17@gmail.com>
2022-10-11 09:34:10 +0200

    first commit

---
+++ age_calculator.js
@@ -0,0 +1,9 @@
+let birth_year = Number(prompt("Enter your birthyear: "));
+let future_year = Number(prompt("Enter the future year : "));
+let age = future_year - birth_year;
+
+console.log(age-1);
+
+window.alert("I will be either " + (age - 1) + " or " + age + " in " +future_year);
+document.write(age);
+window.alert(age);
